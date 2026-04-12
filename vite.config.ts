7498fac363f334ab48120a683d33316a28a4c910
dboy3156 <danerez5@gmail.com>
2026-04-13 02:07:33 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import Compression from 'vite-plugin-compression';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const { version } = JSON.parse(readFileSync("./package.json", "utf-8")) as { version: string };
 
 const sentryPlugin =
@@ -19,7 +20,7 @@ const sentryPlugin =
     : [];
 
 export default defineConfig({
-  plugins: [viteCompression(), react(), ...sentryPlugin],
+  plugins: [react(), ...sentryPlugin],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
