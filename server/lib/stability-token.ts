9ba500aaca87c35e0be28a76587a6687011b6299
dboy3156 <danerez5@gmail.com>
2026-04-12 07:23:28 +0300

    Update stability-token.ts

--- server/lib/stability-token.ts
+++ server/lib/stability-token.ts
@@ -1,3 +1,2 @@
-import { randomBytes } from "crypto";
-
-export const STABILITY_TOKEN = randomBytes(32).toString("hex");
+export const STABILITY_TOKEN =
+  process.env.STABILITY_TOKEN ?? randomBytes(32).toString("hex");
